c3f613d15b780166fa0abc4152d267cee9ea1be7
Joe Lee <blueberrymozart@gmail.com>
2015-03-26 18:49:30 -0400

    add scaffold files

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'bbmoz:gulp',
-  version: '0.0.9',
+  version: '0.0.10',
   summary: 'Easy scaffolding for gulp',
   git: 'https://github.com/bbmoz/meteor-gulp.git',
   documentation: 'README.md'
@@ -14,4 +14,6 @@ Npm.depends({
 Package.onUse(function (api) {
   api.versionsFrom('1.0.5');
   api.addFiles('gulp.js', 'server');
+  api.addFiles('./scaffold/gulpfile.js', 'server');
+  api.addFiles('./scaffold/package.json', 'server');
 });
