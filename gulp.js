8afb70f28476b6b545e8f1cd1759eacc33c1822a
Joe Lee <blueberrymozart@gmail.com>
2015-03-26 19:14:03 -0400

    cleanup spacing

--- gulp.js
+++ gulp.js
@@ -7,7 +7,7 @@
         newGulpDir = process.env.PWD + '/.gulp/',
         gulpFileContent, packageFileContent;
 
-    gulpFileContent = "var gulp = require('gulp'),\n\tjshint = require('gulp-jshint');\n\nvar srcs = {\n\tlint: ['./../client/templates/**/*.js', './../lib/**/*.js', './../server/**/*.js']\n};\ngulp.task('lint', function () {\n\treturn gulp.src(srcs.lint)\n\t\t.pipe(jshint())\n\t\t.pipe(jshint.reporter('default'))\n\t\t.pipe(jshint.reporter('fail'));\n});\ngulp.task('watch', function () {\n\tgulp.watch(srcs.lint, ['lint']);\n});\ngulp.task('default', ['lint', 'watch']);";
+    gulpFileContent = "var gulp = require('gulp'),\n\tjshint = require('gulp-jshint');\n\nvar srcs = {\n\tlint: ['./../client/templates/**/*.js', './../lib/**/*.js', './../server/**/*.js']\n};\n\ngulp.task('lint', function () {\n\treturn gulp.src(srcs.lint)\n\t\t.pipe(jshint())\n\t\t.pipe(jshint.reporter('default'))\n\t\t.pipe(jshint.reporter('fail'));\n});\n\ngulp.task('watch', function () {\n\tgulp.watch(srcs.lint, ['lint']);\n});\n\ngulp.task('default', ['lint', 'watch']);";
     packageFileContent = '{\n\t"name": "task-runner",\n\t"version": "0.0.1",\n\t"description": "gulp plugins",\n\t"main": "gulpfile.js",\n\t"devDependencies": {\n\t\t"gulp": "latest",\n\t\t"jshint": "latest"\n\t}\n}';
 
     async.series({
