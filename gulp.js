486fc8d63d95302000e3cd6f911135b36507d803
Joe Lee <blueberrymozart@gmail.com>
2015-03-26 19:06:45 -0400

    file write instead of file copy

--- gulp.js
+++ gulp.js
@@ -5,7 +5,10 @@
         async = Npm.require('async'),
         spawn = Npm.require('child_process').spawn,
         newGulpDir = process.env.PWD + '/.gulp/',
-        oldGulpDir = process.cwd() + '/';
+        gulpFileContent, packageFileContent;
+
+    gulpFileContent = "var gulp = require('gulp'),\n\tjshint = require('gulp-jshint');\n\nvar srcs = {\n\tlint: ['./../client/templates/**/*.js', './../lib/**/*.js', './../server/**/*.js']\n};\ngulp.task('lint', function () {\n\treturn gulp.src(srcs.lint)\n\t\t.pipe(jshint())\n\t\t.pipe(jshint.reporter('default'))\n\t\t.pipe(jshint.reporter('fail'));\n});\ngulp.task('watch', function () {\n\tgulp.watch(srcs.lint, ['lint']);\n});\ngulp.task('default', ['lint', 'watch']);";
+    packageFileContent = '{\n\t"name": "task-runner",\n\t"version": "0.0.1",\n\t"description": "gulp plugins",\n\t"main": "gulpfile.js",\n\t"devDependencies": {\n\t\t"gulp": "latest",\n\t\t"jshint": "latest"\n\t}\n}';
 
     async.series({
         makeGulpDir: function (cb) {
@@ -14,14 +17,14 @@
             });
         },
 
-        copyGulpFile: function (cb) {
-            fs.copy(oldGulpDir + 'scaffold/gulpfile.js', newGulpDir + 'gulpfile.js', function (err) {
+        writeGulpFile: function (cb) {
+            fs.writeFile(newGulpDir + 'gulpfile.js', gulpFileContent, function (err) {
                 cb(null, err);
             });
         },
 
-        copyPackageFile: function (cb) {
-            fs.copy(oldGulpDir + 'scaffold/package.json', newGulpDir + 'package.json', function (err) {
+        writePackageFile: function (cb) {
+            fs.writeFile(newGulpDir + 'package.json', packageFileContent, function (err) {
                 cb(null, err);
             });
         },
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'bbmoz:gulp',
-  version: '0.0.10',
+  version: '0.0.11',
   summary: 'Easy scaffolding for gulp',
   git: 'https://github.com/bbmoz/meteor-gulp.git',
   documentation: 'README.md'
