108095c4ed05f9d19148d5e99b5361b3deda3f5a
Joe Lee <blueberrymozart@gmail.com>
2015-03-28 03:36:43 -0400

    remove jscs

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'bbmoz:gulp',
-    version: '0.1.3',
+    version: '0.1.4',
     summary: 'Easy scaffolding for gulp',
     git: 'https://github.com/bbmoz/meteor-gulp.git',
     documentation: 'pkg.md'
@@ -13,5 +13,5 @@ Npm.depends({
 
 Package.onUse(function (api) {
     api.versionsFrom('1.0.5');
-    api.addFiles(['plugin/gulp.js', 'plugin/package.json', 'plugin/gulpfile.temp', 'plugin/.jshintrc', 'plugin/.jscsrc', 'plugin/.csslintrc', 'plugin/.gitignore'], 'server');
+    api.addFiles(['plugin/gulp.js', 'plugin/package.json', 'plugin/gulpfile.temp', 'plugin/.jshintrc', 'plugin/.csslintrc', 'plugin/.gitignore'], 'server');
 });
