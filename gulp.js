ae3b61166f379abdc25135d9e8b06797a0695847
Joe Lee <blueberrymozart@gmail.com>
2015-03-27 23:22:25 -0400

    revise gulp file logic and prettify outputs from jshint, include jscs for additional code quality

--- gulp.js
+++ gulp.js
@@ -4,11 +4,8 @@
     var fs = Npm.require('fs.extra'),
         async = Npm.require('async'),
         spawn = Npm.require('child_process').spawn,
-        newGulpDir = process.env.PWD + '/.gulp/',
-        gulpFileContent, packageFileContent;
+        newGulpDir = process.env.PWD + '/.gulp/';
 
-    gulpFileContent = "var gulp = require('gulp'),\n\tjshint = require('gulp-jshint');\n\nvar srcs = {\n\tlint: ['./../client/templates/**/*.js', './../lib/**/*.js', './../server/**/*.js']\n};\n\ngulp.task('lint', function () {\n\treturn gulp.src(srcs.lint)\n\t\t.pipe(jshint())\n\t\t.pipe(jshint.reporter('default'))\n\t\t.pipe(jshint.reporter('fail'));\n});\n\ngulp.task('watch', function () {\n\tgulp.watch(srcs.lint, ['lint']);\n});\n\ngulp.task('default', ['lint', 'watch']);";
-    packageFileContent = '{\n\t"name": "task-runner",\n\t"version": "0.0.1",\n\t"description": "gulp plugins",\n\t"main": "gulpfile.js",\n\t"devDependencies": {\n\t\t"gulp": "latest",\n\t\t"jshint": "latest"\n\t}\n}';
 
     async.series({
         makeGulpDir: function (cb) {
@@ -17,20 +14,44 @@
             });
         },
 
-        writeGulpFile: function (cb) {
-            fs.writeFile(newGulpDir + 'gulpfile.js', gulpFileContent, function (err) {
-                cb(null, err);
-            });
-        },
+        copyFiles: function (cb) {
+            async.parallel({
+                copyPackageFile: function (cbFile) {
+                    fs.copy('package.json', newGulpDir + 'package.json', function (err) {
+                        cbFile(null, err);
+                    });
+                },
 
-        writePackageFile: function (cb) {
-            fs.writeFile(newGulpDir + 'package.json', packageFileContent, function (err) {
-                cb(null, err);
+                copyGulpFile: function (cbFile) {
+                    fs.copy('gulpfile.js', newGulpDir + 'gulpfile.js', function (err) {
+                        cbFile(null, err);
+                    });
+                },
+
+                copyJshintrcFile: function (cbFile) {
+                    fs.copy('.jshintrc', newGulpDir + '.jshintrc', function (err) {
+                        cbFile(null, err);
+                    });
+                },
+
+                copyJscsrcFile: function (cbFile) {
+                    fs.copy('.jscsrc', newGulpDir + '.jscsrc', function (err) {
+                        cbFile(null, err);
+                    });
+                },
+
+                copyGitignoreFile: function (cbFile) {
+                    fs.copy('.gitignore', newGulpDir + '.gitignore', function (err) {
+                        cbFile(null, err);
+                    });
+                }
+            }, function (err) {
+                cb(null, err)
             });
         },
 
         installGulpPlugins: function (cb) {
-            var npmInstall = spawn('npm', ['install'], {
+            var npmInstall = spawn('npm', ['install', '--save-dev'], {
                 cwd: newGulpDir,
                 stdio: 'inherit'
             });
@@ -39,6 +60,20 @@
                     cb(null, 'SUCCESS: npm install in .gulp/');
                 } else {
                     cb('FAIL: npm install in .gulp/');
+                }
+            });
+        },
+
+        installGulpGlobal: function (cb) {
+            var npmInstall = spawn('npm', ['install', '-g', 'gulp'], {
+                cwd: newGulpDir,
+                stdio: 'inherit'
+            });
+            npmInstall.on('exit', function (code) {
+                if (code === 0) {
+                    cb(null, 'SUCCESS: npm -g install gulp');
+                } else {
+                    cb('FAIL: npm -g install gulp');
                 }
             });
         }
--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
-  name: 'bbmoz:gulp',
-  version: '0.0.13',
-  summary: 'Easy scaffolding for gulp',
-  git: 'https://github.com/bbmoz/meteor-gulp.git',
-  documentation: 'README.md'
+    name: 'bbmoz:gulp',
+    version: '0.0.14',
+    summary: 'Easy scaffolding for gulp',
+    git: 'https://github.com/bbmoz/meteor-gulp.git',
+    documentation: 'README.md'
 });
 
 Npm.depends({
-  "fs.extra": "1.3.2",
-  "async": "0.9.0"
+    "fs.extra": "1.3.2",
+    "async": "0.9.0"
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.0.5');
-  api.addFiles('gulp.js', 'server');
+    api.versionsFrom('1.0.5');
+    api.addFiles(['gulp.js', 'package.json', 'gulpfile.js', '.jshintrc', 'jscsrc', '.gitignore'], 'server');
 });
